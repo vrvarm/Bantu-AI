f3595ce68295bd35b38b972c015080824c834e43
Ravi Varma Vathsavai <vrvarmaink@gmail.com>
2024-12-31 11:39:11 -0600

    A new start for this project

--- Logging_config.py
+++
@@ -1,6 +0,0 @@
-import logging#-
-
-# Set up logging for the project
-def setup_logging():
-    logging.basicConfig(filename='project_errors.log', level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
--- text_to_speech.py
+++
@@ -1,6 +0,0 @@
-import pyttsx3
-
-def text_to_speech(text):
-    engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
